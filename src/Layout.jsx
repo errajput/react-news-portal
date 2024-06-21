fa76d8ecc86dcab084242d89dfa9b036b17a14d0
errajput <dishacsevira@gmail.com>
2024-06-21 10:51:30 +0530

    import ArticleList file

---
+++ src/Layout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ArticleList from "./ArticleList.jsx";
+
+const Layout = () => {
+    return (
+        <div>
+            <ArticleList />
+        </div>
+    );
+};
+
+export default Layout;
